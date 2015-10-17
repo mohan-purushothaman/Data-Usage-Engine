f407414370150d35e70a9bfb33a18eac006e6f29
Mohan Purushothaman <>
2015-10-18 00:49:42 +0530

    initial commit

--- src/test/java/org/verizon/mohan/springweb/test/AddTest.java
+++ src/test/java/org/verizon/mohan/springweb/test/AddTest.java
@@ -34,7 +34,7 @@ public class AddTest extends TestCase {
     // TODO add test methods here. The name must begin with 'test'. For example:
     // public void testHello() {}
     public void testAdd() {
-        Assert.assertEquals(132,(int) c.test(11, 120));
+        Assert.assertEquals(132,(int) c.test(11, 121));
     }
 
 }
