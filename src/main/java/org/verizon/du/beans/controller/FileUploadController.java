d08c8a56a555ded4705f73c27af8e81969bcc47b
Administrator <Administrator@scsbwin-910822.ITTPA.EBIZ.VERIZON.COM>
2015-10-29 06:20:37 -0700

    Checking in the input field

--- src/main/java/org/verizon/du/beans/controller/FileUploadController.java
+++ src/main/java/org/verizon/du/beans/controller/FileUploadController.java
@@ -8,10 +8,13 @@ package org.verizon.du.beans.controller;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
  
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.verizon.du.core.BaseConfig;
+import org.verizon.du.core.DataUsage;
+import org.verizon.du.core.Engine;
  
 /**
  *
@@ -26,7 +31,8 @@ import org.verizon.du.core.BaseConfig;
  */
 @Controller
 public class FileUploadController {
- 
+   @Autowired
+    Engine engine;
     private static final Logger logger = LoggerFactory
             .getLogger(FileUploadController.class);
  
@@ -41,20 +47,28 @@ public class FileUploadController {
             long startTime=System.currentTimeMillis();
         if (!file.isEmpty()) {
             try {
-                byte[] bytes = file.getBytes();
                 BufferedInputStream ipStream=new BufferedInputStream(file.getInputStream(),BaseConfig.STREAM_BUFFER_SIZE);
                 BufferedReader ipReader = new BufferedReader(new InputStreamReader(ipStream),BaseConfig.BUFFER_SIZE);
                 while (ipReader.ready()){
-                String line = ipReader.readLine();
-               }
-              logger.info("Time taken----"+(System.currentTimeMillis()-startTime)+"ms");
+                    String line=ipReader.readLine();
+                 DataUsage du= createDataUsage(line);
+                 }
+                logger.info("Time taken----"+(System.currentTimeMillis()-startTime)+"ms");
  
             } catch (Exception e) {
-                throw new Exception("Exception occured while reading file"+e);
+                logger.error("Exception", e);
+                throw e;
             }
         } else {
             throw new Exception("File is empty");
         }
     }
-     
+     private DataUsage createDataUsage(String usageString) throws Exception {
+        String [] split=usageString.split(BaseConfig.SPLIT_STRING);
+        return new DataUsage(split[0], split[1], split[2], Long.parseLong(split[3]), parseDate(split[4]), parseDate(split[5]));
+    }
+     SimpleDateFormat dff=new SimpleDateFormat(BaseConfig.DATE_FORMAT);
+      private Date parseDate(String dateString) throws Exception{
+        return dff.parse(dateString);
+    }
 }
