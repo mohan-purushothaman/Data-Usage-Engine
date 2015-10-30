64d7d6ecc85a552a04da5eb97a53831f2174c7f3
Administrator <Administrator@scsbwin-751387.ITTPA.EBIZ.VERIZON.COM>
2015-10-30 03:29:33 -0700

    added test case for simple alert system

--- src/test/java/org/verizon/du/test/SimpleAlertSystemTest.java
+++ src/test/java/org/verizon/du/test/SimpleAlertSystemTest.java
@@ -8,6 +8,7 @@ package org.verizon.du.test;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 import org.verizon.du.beans.impl.SimpleAlertSystem;
+import org.verizon.du.core.Alert;
 import org.verizon.du.core.Customer;
 
 /**
@@ -35,32 +36,35 @@ public class SimpleAlertSystemTest extends TestCase {
      */
     public void testProcessAlerts75() {
         System.out.println("processAlerts");
-        Customer customer = new Customer("testCustomer", null, null, 105*1024*1024*1024);
-        customer.addToMonthlyUsage(230);
+        Customer customer = new Customer("testCustomer", null, null, 112L*1024*1024*1024,"a@b",123);
+        customer.addToMonthlyUsage(1L*1024*1024*1024);
         SimpleAlertSystem instance = new SimpleAlertSystem();
-       // Alert a  = instance.processAlerts(customer);
-        //Assert.assertNotNull(a);
-        //int value = Alert.
-          //      assertEquals(75);        // TODO review the generated test code and remove the default call to fail.
+        Alert a  = instance.processAlerts(customer);
+        Assert.assertNotNull(a);
+        int value = a.getAlertPercentage();
+        assertEquals(value,75);        // TODO review the generated test code and remove the default call to fail.
         
     }
     
     public void testProcessAlertsInsideLimit() {
         System.out.println("processAlerts");
-        Customer customer = new Customer("testCustomer", null, null, 1123400L);
+        Customer customer = new Customer("testCustomer", null, null, 1123400L,"a@b",1234567890);
         customer.addToMonthlyUsage(2230L);
         SimpleAlertSystem instance = new SimpleAlertSystem();
-        instance.processAlerts(customer);
+        Alert a  = instance.processAlerts(customer);
+        Assert.assertNull(a);
         // TODO review the generated test code and remove the default call to fail.
-        
     }
     
     public void testProcessAlerts85() {
         System.out.println("processAlerts");
-        Customer customer = new Customer("testCustomer", null, null, 128*1024*1024*1024);
-        customer.addToMonthlyUsage(2230L);
+        Customer customer = new Customer("testCustomer", null, null, 128L*1024*1024*1024,"a@b",1234567890);
+        customer.addToMonthlyUsage((long) 2*1024*1024*1024);
         SimpleAlertSystem instance = new SimpleAlertSystem();
-        instance.processAlerts(customer);
+        Alert a  = instance.processAlerts(customer);
+        Assert.assertNotNull(a);
+        int value = a.getAlertPercentage();
+        assertEquals(value,85);
         // TODO review the generated test code and remove the default call to fail.
         
     }
